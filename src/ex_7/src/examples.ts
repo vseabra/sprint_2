26043c75f390e9d2bf28c22d9c863bec787ba672
Vinícius Seabra <vseabra@protonmail.com>
2021-11-06 18:54:15 -0300

    fix typo (unfiltedList)

--- src/ex_7/src/examples.ts
+++ src/ex_7/src/examples.ts
@@ -1,10 +1,10 @@
 import NumericList from "./main"
 import NumericListImperative from "./imperative"
 
-const unfiltedList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "não é um número", {} ]
+const unfilteredList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "não é um número", {} ]
 
-const numericList = new NumericList(unfiltedList)
-const numericListImperative = new NumericListImperative(unfiltedList)
+const numericList = new NumericList(unfilteredList)
+const numericListImperative = new NumericListImperative(unfilteredList)
 
 console.log(numericList.smallestLargestAverage) 
 console.log(numericListImperative.smallestLargestAverage) 
